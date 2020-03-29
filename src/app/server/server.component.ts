245b840ec941c706efe1d38efded9fb724ddff9a
vidalive <vidalive@gmail.com>
2020-03-28 22:04:38 -0600

    created servers component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,18 +1,12 @@
 
-// import the component from @angular/core package
 import { Component } from '@angular/core';
 
-
-// this is a decorator
-@Component(
-    // and we pass a JS abject to it 
-    {
-
-    // and any JS object has a propery and a value
+@Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
-}
-)
+  templateUrl: './server.component.html' 
+})
+
+
 export class ServerComponent {
 
 }
